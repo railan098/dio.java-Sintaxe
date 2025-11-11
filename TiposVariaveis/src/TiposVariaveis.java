0159a48daf44048ccfe7c19dd86f27ab1be1023c
railan098 <alvesdasilvarailan4@gmail.com>
2025-11-11 10:30:05 -0300

    alteração no projeto renomeando para TiposVariaveis

---
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        System.out.println(numeroCurto2);
+
+        int a = 2;
+        a = 3;
+
+        double valor = 3.14;
+
+
+        System.out.println(valor);
+
+
+
+    }
+}
